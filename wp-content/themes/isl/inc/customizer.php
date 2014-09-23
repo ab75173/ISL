eb0fb37091c7f2c5d35b43bd2fcabcaaf985eb02
ab75173 <adam.p.barrett@gmail.com>
2014-09-23 14:31:51 -0400

    one page format

---
+++ wp-content/themes/isl/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * isl Theme Customizer
+ *
+ * @package isl
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function isl_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'isl_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function isl_customize_preview_js() {
+	wp_enqueue_script( 'isl_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'isl_customize_preview_js' );
